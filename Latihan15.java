cead86b586135799b67d0384c0f7a51b373f3d3f
Aziskomara <55784203+Aziskomara@users.noreply.github.com>
2019-09-26 15:41:28 +0700

    Add files via upload

---
+++ Latihan15.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan15;
+
+/**
+ * Nama     : Azis komara
+ * Kelas    : IF-1
+ * Nim      : 10118029
+ * deskiripsi program   : Penerapan Logika 2
+ * 
+ * @author asus
+ */
+public class Latihan15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("(a && b) = " + !(a && b));
+    }
+    
+}
